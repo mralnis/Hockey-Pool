0d8f9ba62d46784e0a4a004ed783a201c13a156a
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-02-21 22:49:41 +0200

    picture

--- HockeyPool/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ HockeyPool/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -21,24 +21,6 @@ namespace Microsoft.AspNetCore.Routing
 
             var accountGroup = endpoints.MapGroup("/Account");
 
-            accountGroup.MapPost("/PerformExternalLogin", (
-                HttpContext context,
-                [FromServices] SignInManager<ApplicationUser> signInManager,
-                [FromForm] string provider,
-                [FromForm] string returnUrl) =>
-            {
-                IEnumerable<KeyValuePair<string, StringValues>> query = [
-                    new("ReturnUrl", returnUrl),
-                    new("Action", ExternalLogin.LoginCallbackAction)];
-
-                var redirectUrl = UriHelper.BuildRelative(
-                    context.Request.PathBase,
-                    "/Account/ExternalLogin",
-                    QueryString.Create(query));
-
-                var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-                return TypedResults.Challenge(properties, [provider]);
-            });
 
             accountGroup.MapPost("/Logout", async (
                 ClaimsPrincipal user,
