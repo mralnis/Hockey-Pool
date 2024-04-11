da6266d3fc589aaf7d193e28a43474521f9b3f34
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-04-11 19:16:06 +0300

    Turnament seed

--- Infrastructure/Data/DbContext.cs
+++ Infrastructure/Data/DbContext.cs
@@ -1,7 +1,6 @@
 using HockeyPool.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
 namespace HockeyPool.Infrastructure.Data
 {
@@ -15,28 +14,44 @@ namespace HockeyPool.Infrastructure.Data
 
         public void Seed()
         {
-            if (!this.Countries.Any())
+            if (!Countries.Any())
             {
-                this.Countries.Add(new Country { Name = "Latvija", FlagCode = "LV" });
-                this.Countries.Add(new Country { Name = "ASV", FlagCode = "US" });
-                this.Countries.Add(new Country { Name = "Zviedrija", FlagCode = "SE" });
-                this.Countries.Add(new Country { Name = "Kanâda", FlagCode = "CA" });
-                this.Countries.Add(new Country { Name = "Kazahstâna", FlagCode = "KZ" });
-                this.Countries.Add(new Country { Name = "Itâlija", FlagCode = "IT" });
-                this.Countries.Add(new Country { Name = "Norvçìija", FlagCode = "NO" });
-                this.Countries.Add(new Country { Name = "Somija", FlagCode = "FI" });
-                this.Countries.Add(new Country { Name = "Vâcija", FlagCode = "DE" });
-                this.Countries.Add(new Country { Name = "Èehija", FlagCode = "CZ" });
-                this.Countries.Add(new Country { Name = "Lielbritânija", FlagCode = "GB" });
-                this.Countries.Add(new Country { Name = "Austrija", FlagCode = "AT" });
-                this.Countries.Add(new Country { Name = "Ðveice", FlagCode = "CH" });
-                this.Countries.Add(new Country { Name = "Dânija", FlagCode = "DK" });
-                this.Countries.Add(new Country { Name = "Francija", FlagCode = "FR" });
-                this.Countries.Add(new Country { Name = "Slovâkija", FlagCode = "SK" });
-                this.Countries.Add(new Country { Name = "Polija", FlagCode = "PL" });
+                Countries.Add(new Country { Name = "Latvija", FlagCode = "LV" });
+                Countries.Add(new Country { Name = "ASV", FlagCode = "US" });
+                Countries.Add(new Country { Name = "Zviedrija", FlagCode = "SE" });
+                Countries.Add(new Country { Name = "Kanâda", FlagCode = "CA" });
+                Countries.Add(new Country { Name = "Kazahstâna", FlagCode = "KZ" });
+                Countries.Add(new Country { Name = "Itâlija", FlagCode = "IT" });
+                Countries.Add(new Country { Name = "Norvçìija", FlagCode = "NO" });
+                Countries.Add(new Country { Name = "Somija", FlagCode = "FI" });
+                Countries.Add(new Country { Name = "Vâcija", FlagCode = "DE" });
+                Countries.Add(new Country { Name = "Èehija", FlagCode = "CZ" });
+                Countries.Add(new Country { Name = "Lielbritânija", FlagCode = "GB" });
+                Countries.Add(new Country { Name = "Austrija", FlagCode = "AT" });
+                Countries.Add(new Country { Name = "Ðveice", FlagCode = "CH" });
+                Countries.Add(new Country { Name = "Dânija", FlagCode = "DK" });
+                Countries.Add(new Country { Name = "Francija", FlagCode = "FR" });
+                Countries.Add(new Country { Name = "Slovâkija", FlagCode = "SK" });
+                Countries.Add(new Country { Name = "Polija", FlagCode = "PL" });
 
-                this.SaveChanges();
+                SaveChanges();
+            }
+            if (!Tournaments.Any(_ => _.Name == "IIHF 2024"))
+            {
+                Tournaments.Add(new Tournament 
+                {
+                    CountryId = 10, 
+                    StartDate = new DateTime(2024, 05, 10), 
+                    EndDate = new DateTime(2024, 05, 27), 
+                    MatchupClosingTime = 15, 
+                    Name = "IIHF 2024", 
+                    IsActive = true, 
+                    PointsForPerfect = 5, 
+                    PointForDifference = 3, 
+                    PointsForWinnerOnly = 1 
+                });
 
+                SaveChanges();
             }
         }
     }
