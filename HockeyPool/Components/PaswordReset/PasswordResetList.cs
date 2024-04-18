0b56b27a5160672b56adadb5ad2242454689c693
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-04-19 00:31:58 +0300

    InitalPasswordReset

---
+++ HockeyPool/Components/PaswordReset/PasswordResetList.cs
@@ -0,0 +1,7 @@
+﻿namespace HockeyPool.Components.PaswordReset
+{
+    public static class PasswordResetList
+    {
+        public static  Dictionary<Guid,int> PasswordToReset = new Dictionary<Guid,int>();
+    }
+}
