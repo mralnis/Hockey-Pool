f048bdbe83a409a1222098e4622533f5fa275801
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-05-06 20:53:14 +0300

    Missing

---
+++ Infrastructure/Data/Migrations/20240506174558_LogAditionalData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HockeyPool.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogAditionalData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BrowserVersion",
+                table: "PredictionLogs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "PredictionLogs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BrowserVersion",
+                table: "PredictionLogs");
+
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "PredictionLogs");
+        }
+    }
+}
