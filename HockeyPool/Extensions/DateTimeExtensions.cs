832740129e64891f7fd0cc89ffff6209f668fc23
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-05-21 12:39:42 +0300

    Edgara variants

--- HockeyPool/Extensions/DateTimeExtensions.cs
+++ HockeyPool/Extensions/DateTimeExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class DateTimeExtensions
     {
-        public static string DayOfWeekLv(this DateTime date)
+        public static string DayOfWeekLvShort(this DateTime date)
         {
             return date.DayOfWeek switch
             {
@@ -13,6 +13,21 @@
                 DayOfWeek.Friday => "Pk",
                 DayOfWeek.Saturday => "S",
                 DayOfWeek.Sunday => "Sv",
+                _ => "",
+            };
+        }
+
+        public static string DayOfWeekLv(this DateTime date)
+        {
+            return date.DayOfWeek switch
+            {
+                DayOfWeek.Monday => "Pirmdiena",
+                DayOfWeek.Tuesday => "Otrdiena",
+                DayOfWeek.Wednesday => "Trešdiena",
+                DayOfWeek.Thursday => "Ceturtdiena",
+                DayOfWeek.Friday => "Piektdiena",
+                DayOfWeek.Saturday => "Sestdiena",
+                DayOfWeek.Sunday => "Svētdiena",
                 _ => "",
             };
         }
