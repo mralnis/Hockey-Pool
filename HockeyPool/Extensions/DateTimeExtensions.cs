ee17eda930111fb72e414c6dd2c93739d41b090d
Didzis Stivriņš <didzis.stivrins@intressebevakaren.se>
2024-05-21 12:11:48 +0300

    Added DayOfWeek for matchups

---
+++ HockeyPool/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace HockeyPool.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string DayOfWeekLv(this DateTime date)
+        {
+            return date.DayOfWeek switch
+            {
+                DayOfWeek.Monday => "P",
+                DayOfWeek.Tuesday => "O",
+                DayOfWeek.Wednesday => "T",
+                DayOfWeek.Thursday => "C",
+                DayOfWeek.Friday => "Pk",
+                DayOfWeek.Saturday => "S",
+                DayOfWeek.Sunday => "Sv",
+                _ => "",
+            };
+        }
+    }
+}
